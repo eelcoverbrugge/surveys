a7fc5d0217f66b4f3d90ff10c0a0f5d51430254d
veelco <veelco6@gmail.com>
2020-03-19 11:48:09 +0100

    Vragen toegevoegd

--- app/Question.php
+++ app/Question.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function questionnaire()
+    {
+        return $this->belongsTo(Questionnaire::class);
+    }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
 }
