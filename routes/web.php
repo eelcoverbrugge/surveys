0a4c5ee40ad3d5aa327cdcb2fb860b66ebfb7c84
veelco <veelco6@gmail.com>
2020-03-19 10:45:41 +0100

    Maken van Questionaires toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/questionnaires/create', 'QuestionnaireController@create');
+Route::post('/questionnaires', 'QuestionnaireController@store');
+Route::get('/questionnaires/{questionnaire}', 'QuestionnaireController@show');
